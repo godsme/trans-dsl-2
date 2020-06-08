1867cd0742183a5473f40c0b3812f5edb80b2440
Darwin Yuan <darwin_yuan@hotmail.com>
2020-06-09 04:28:32 +0800

    changed sphnix

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -31,7 +31,6 @@ release = '2.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    #"sphinx_rtd_theme"
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -55,7 +54,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-#html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,

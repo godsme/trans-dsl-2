432967e814b3fa1dfb74880d8045f62f1f582925
Darwin Yuan <darwin_yuan@hotmail.com>
2020-06-15 19:05:28 +0800

    added trans-dsl all-in-one header

---
+++ include/trans-dsl/trans-dsl.h
@@ -0,0 +1,17 @@
+//
+// Created by Darwin Yuan on 2020/6/15.
+//
+
+#ifndef TRANS_DSL_2_TRANS_DSL_H
+#define TRANS_DSL_2_TRANS_DSL_H
+
+#include <trans-dsl/sched/helper/SyncActionHelper.h>
+#include <trans-dsl/sched/helper/AsyncActionHelper.h>
+#include <trans-dsl/sched/helper/SequentialHelper.h>
+#include <trans-dsl/sched/helper/ConcurrentHelper.h>
+#include <trans-dsl/sched/helper/TimerGuardHelper.h>
+#include <trans-dsl/sched/helper/OptionalHelper.h>
+#include <trans-dsl/sched/helper/SwitchCaseHelper.h>
+#include <trans-dsl/sched/helper/ProcedureHelper.h>
+
+#endif //TRANS_DSL_2_TRANS_DSL_H

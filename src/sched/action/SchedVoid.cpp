6b107559ab193beac32a26111c7e301cddee3dde
Darwin Yuan <darwin_yuan@hotmail.com>
2020-06-16 12:43:23 +0800

    added implmentation of void

--- src/sched/action/SchedVoid.cpp
+++ src/sched/action/SchedVoid.cpp
@@ -4,26 +4,49 @@
 
 #include <trans-dsl/sched/action/SchedVoid.h>
 #include <trans-dsl/utils/ActionStatus.h>
+#include <trans-dsl/sched/concept/RuntimeContextAutoSwitch.h>
 
 TSL_NS_BEGIN
 
 ///////////////////////////////////////////////////////////////////////////////
+namespace {
+   struct AutoSwitch
+      : private RuntimeContext
+      , RuntimeContextAutoSwitch {
+      AutoSwitch(TransactionContext& transactionContext)
+         : RuntimeContextAutoSwitch(transactionContext, *this) {}
+   };
+}
+
+///////////////////////////////////////////////////////////////////////////////
+#define AUTO_SWITCH() AutoSwitch autoSwitch__{context}
+
+///////////////////////////////////////////////////////////////////////////////
+#define __CALL(call) do {                                \
+   AUTO_SWITCH();                                        \
+   ActionStatus status = ROLE(SchedAction).call;         \
+   return status.isWorking() ? status : Result::SUCCESS; \
+} while(0)
+
+///////////////////////////////////////////////////////////////////////////////
 auto SchedVoid::exec(TransactionContext& context) -> Status {
-   return FATAL_BUG;
+   __CALL(exec(context));
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 auto SchedVoid::handleEvent(TransactionContext& context, const Event& event) -> Status {
-   return FATAL_BUG;
+   __CALL(handleEvent(context, event));
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 auto SchedVoid::stop(TransactionContext& context, Status cause) -> Status {
-   return FATAL_BUG;
+   __CALL(stop(context, cause));
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 auto SchedVoid::kill(TransactionContext& context, Status cause) -> void {
+   AUTO_SWITCH();
+   return ROLE(SchedAction).kill(context, cause);
 }
 
 

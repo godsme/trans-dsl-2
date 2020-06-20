2816bf63b59c025b84b8e73336a0bcc86405f5e4
Darwin Yuan <darwin_yuan@hotmail.com>
2020-06-20 15:33:18 +0800

    added loop concept

--- include/trans-dsl/sched/helper/LoopHelper.h
+++ include/trans-dsl/sched/helper/LoopHelper.h
@@ -181,7 +181,7 @@ namespace details {
    template<uint32_t V_MAX_TIMES, typename ... T_ACTIONS>
    struct LOOP__ {
       static_assert(sizeof...(T_ACTIONS) > 0, "loop cannot be empty");
-      using Actions = typename GenericLoop_<0, 0, 0, void, T_ACTIONS...>::Inner;
+      using Actions = typename GenericLoop_<0, 0, 0, VOID_PLACEHOLDER T_ACTIONS...>::Inner;
       struct Inner : private Actions, SchedLoop {
       private:
          OVERRIDE(getMaxTime() const -> uint32_t) {

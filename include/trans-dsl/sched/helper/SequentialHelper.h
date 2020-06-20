2c2d00528e8618e4b3c2aa5126e8157ee7540e54
Darwin Yuan <darwin_yuan@hotmail.com>
2020-06-20 16:34:45 +0800

    refactored concept

--- include/trans-dsl/sched/helper/SequentialHelper.h
+++ include/trans-dsl/sched/helper/SequentialHelper.h
@@ -6,81 +6,84 @@
 #define TRANS_DSL_2_SEQUENTIALHELPER_H
 
 #include <trans-dsl/tsl_ns.h>
-#include <cstddef>
-#include <algorithm>
 #include <trans-dsl/sched/action/SchedSequential.h>
 #include <trans-dsl/utils/SeqInt.h>
 #include <trans-dsl/sched/concepts/SchedActionConcept.h>
+#include <cstddef>
+#include <algorithm>
 
 TSL_NS_BEGIN
 
 struct SchedAction;
 
-template<size_t T_SIZE, size_t T_ALIGN, SeqInt T_SEQ  VOID_CONCEPT, typename ... T_ACTIONS>
-struct GenericSequential;
+namespace details {
+   template<size_t T_SIZE, size_t T_ALIGN, SeqInt T_SEQ VOID_CONCEPT, typename ... T_ACTIONS>
+   struct GenericSequential;
 
-template<
-   size_t T_SIZE,
-   size_t T_ALIGN,
-   SeqInt T_SEQ,
-   typename T_HEAD,
-   typename ... T_TAIL>
-struct GenericSequential<
-   T_SIZE,
-   T_ALIGN,
-   T_SEQ
-   ENABLE_C(SchedActionConcept, T_HEAD),
-   T_HEAD,
-   T_TAIL...> {
-   using Action = T_HEAD;
-   using Next =
+   template<
+      size_t T_SIZE,
+      size_t T_ALIGN,
+      SeqInt T_SEQ,
+      typename T_HEAD,
+      typename ... T_TAIL>
+   struct GenericSequential<
+      T_SIZE,
+      T_ALIGN,
+      T_SEQ ENABLE_C(SchedActionConcept, T_HEAD),
+      T_HEAD,
+      T_TAIL...> {
+      using Action = T_HEAD;
+      using Next =
       typename GenericSequential<
          std::max(T_SIZE, sizeof(Action)),
          std::max(T_ALIGN, alignof(Action)),
-         T_SEQ + 1
-         VOID_PLACEHOLDER,
+         T_SEQ + 1 VOID_PLACEHOLDER,
          T_TAIL...>::Inner;
 
-   struct Inner : Next {
-      using Next::cache;
+      struct Inner : Next {
+         using Next::cache;
 
-      auto get(SeqInt seq) -> SchedAction* {
-         return seq == T_SEQ ? new (cache) Action : Next::get(seq);
-      }
+         auto get(SeqInt seq) -> SchedAction * {
+            return seq == T_SEQ ? new(cache) Action : Next::get(seq);
+         }
+      };
    };
-};
 
-template<size_t T_SIZE, size_t T_ALIGN, SeqInt T_SEQ>
-struct GenericSequential<T_SIZE, T_ALIGN, T_SEQ> {
-   struct Inner  {
-      auto get(SeqInt seq) -> SchedAction* {
-         return nullptr;
-      }
-   protected:
-      alignas(T_ALIGN) char cache[T_SIZE];
+   template<size_t T_SIZE, size_t T_ALIGN, SeqInt T_SEQ>
+   struct GenericSequential<T_SIZE, T_ALIGN, T_SEQ> {
+      struct Inner {
+         auto get(SeqInt seq) -> SchedAction * {
+            return nullptr;
+         }
+
+      protected:
+         alignas(T_ALIGN) char cache[T_SIZE];
+      };
    };
-};
 
-template<CONCEPT(SchedActionConcept) ... T_ACTIONS>
-struct SEQUENTIAL__ {
-   enum { NUM_OF_ACTIONS = sizeof...(T_ACTIONS) };
-   static_assert(NUM_OF_ACTIONS >=2, "__sequential must contain at least 2 actions");
-   static_assert(NUM_OF_ACTIONS <= std::numeric_limits<SeqInt>::max(), "too many actions in a __sequential");
+   template<CONCEPT(SchedActionConcept) ... T_ACTIONS>
+   struct Sequential {
+      enum { NUM_OF_ACTIONS = sizeof...(T_ACTIONS) };
+      static_assert(NUM_OF_ACTIONS >= 2, "__sequential must contain at least 2 actions");
+      static_assert(NUM_OF_ACTIONS <= std::numeric_limits<SeqInt>::max(), "too many actions in a __sequential");
 
-   using Actions = typename GenericSequential<0, 0, 0 VOID_PLACEHOLDER, T_ACTIONS...>::Inner;
-   struct Inner : SchedSequential, private Actions {
-   private:
-      OVERRIDE(getNumOfActions() -> SeqInt) {
-         return NUM_OF_ACTIONS;
-      }
-      OVERRIDE(getNext(SeqInt index) -> SchedAction*) {
-         return Actions::get(index);
-      }
+      using Actions = typename GenericSequential<0, 0, 0 VOID_PLACEHOLDER, T_ACTIONS...>::Inner;
+
+      struct Inner : SchedSequential, private Actions {
+      private:
+         OVERRIDE(getNumOfActions()->SeqInt) {
+            return NUM_OF_ACTIONS;
+         }
+
+         OVERRIDE(getNext(SeqInt index) -> SchedAction*) {
+            return Actions::get(index);
+         }
+      };
    };
-};
+}
 
 
-#define __sequential(...) typename TSL_NS::SEQUENTIAL__<__VA_ARGS__>::Inner
+#define __sequential(...) typename TSL_NS::details::Sequential<__VA_ARGS__>::Inner
 
 TSL_NS_END
 

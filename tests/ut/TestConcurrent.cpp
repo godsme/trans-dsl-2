0c545b0c82950298805e46a5e24fcbf13a31b243
Darwin Yuan <darwin_yuan@hotmail.com>
2020-06-28 14:48:11 +0800

    move concurrent test to catch

--- tests/ut/TestConcurrent.cpp
+++ tests/ut/TestConcurrent.cpp
@@ -2,23 +2,20 @@
 // Created by Darwin Yuan on 2020/6/13.
 //
 
-#include <cctest/cctest.h>
+#include <catch.hpp>
 #include <trans-dsl/sched/helper/AsyncActionHelper.h>
 #include "StupidTransactionContext.h"
 #include "SimpleActionsDefs.h"
 #include <trans-dsl/sched/helper/OptionalHelper.h>
 #include <trans-dsl/sched/helper/SyncActionHelper.h>
-#include <iostream>
 #include <trans-dsl/sched/helper/ProcedureHelper.h>
-#include <trans-dsl/sched/helper/SequentialHelper.h>
 #include <trans-dsl/sched/helper/ConcurrentHelper.h>
-#include <array>
 
 namespace {
    using namespace TSL_NS;
 
 
-   FIXTURE(TestConcurrent) {
+   SCENARIO("__concurrent with 2 async actions") {
       __concurrent(__asyn(AsyncAction1), __asyn(AsyncAction2)) action;
 
       StupidTransactionContext context{};
@@ -31,82 +28,70 @@ namespace {
       const EV_NS::ConsecutiveEventInfo eventInfo2{EV_MSG_2, msg2};
       TSL_NS::Event event2{eventInfo2};
 
-      TEST("event1 -> event2 should return SUCCESS") {
-         ASSERT_EQ(Result::CONTINUE, action.exec(context));
-         ASSERT_EQ(Result::CONTINUE, action.handleEvent(context, event2));
-         ASSERT_EQ(Result::SUCCESS, action.handleEvent(context, event1));
+      REQUIRE(Result::CONTINUE == action.exec(context));
+
+      WHEN("event 1 received, should return CONTINUE") {
+         REQUIRE(Result::CONTINUE == action.handleEvent(context, event1));
+         AND_WHEN("event 2 received, should return SUCCESS") {
+            REQUIRE(Result::SUCCESS == action.handleEvent(context, event2));
+         }
       }
 
-      TEST("stop should return FORCE_STOPPED") {
-         ASSERT_EQ(Result::CONTINUE, action.exec(context));
-         ASSERT_EQ(Result::FORCE_STOPPED, action.stop(context, Result::OUT_OF_SCOPE));
+      WHEN("event 2 received, should return CONTINUE") {
+         REQUIRE(Result::CONTINUE == action.handleEvent(context, event2));
+         AND_WHEN("event 1 received, should return SUCCESS") {
+            REQUIRE(Result::SUCCESS == action.handleEvent(context, event1));
+         }
       }
 
-      TEST("after stopped, start again should return FATAL_BUG") {
-         ASSERT_EQ(Result::CONTINUE, action.exec(context));
-         ASSERT_EQ(Result::FORCE_STOPPED, action.stop(context, Result::OUT_OF_SCOPE));
-         ASSERT_EQ(Result::FATAL_BUG, action.exec(context));
+      WHEN("invoking stop, should return FORCE_STOPPED") {
+         REQUIRE(Result::FORCE_STOPPED == action.stop(context, Result::OUT_OF_SCOPE));
+         AND_WHEN("exec again, should return FATAL_BUG") {
+            REQUIRE(Result::FATAL_BUG == action.exec(context));
+         }
+         AND_WHEN("event received, should return FATAL_BUG") {
+            REQUIRE(Result::FATAL_BUG == action.handleEvent(context, event1));
+         }
       }
 
-      TEST("after killed, start again should return FATAL_BUG") {
-         ASSERT_EQ(Result::CONTINUE, action.exec(context));
+      WHEN("killed") {
          action.kill(context, Result::DUPTID);
-         ASSERT_EQ(Result::FATAL_BUG, action.exec(context));
-      }
-
-      TEST("after stopped, handleEvent again should return FATAL_BUG") {
-         ASSERT_EQ(Result::CONTINUE, action.exec(context));
-         ASSERT_EQ(Result::FORCE_STOPPED, action.stop(context, Result::OUT_OF_SCOPE));
-         ASSERT_EQ(Result::FATAL_BUG, action.handleEvent(context, event1));
-      }
-
-      TEST("after killed, handleEvent again should return FATAL_BUG") {
-         ASSERT_EQ(Result::CONTINUE, action.exec(context));
-         action.kill(context, Result::DUPTID);
-         ASSERT_EQ(Result::FATAL_BUG, action.handleEvent(context, event1));
-      }
-
-      TEST("after stopped, handleEvent again should return FATAL_BUG") {
-         ASSERT_EQ(Result::CONTINUE, action.exec(context));
-         ASSERT_EQ(Result::FORCE_STOPPED, action.stop(context, Result::OUT_OF_SCOPE));
-         ASSERT_EQ(Result::FATAL_BUG, action.handleEvent(context, event2));
-      }
-
-      TEST("event2 -> event1 should return SUCCESS") {
-         ASSERT_EQ(Result::CONTINUE, action.exec(context));
-         ASSERT_EQ(Result::CONTINUE, action.handleEvent(context, event1));
-         ASSERT_EQ(Result::SUCCESS, action.handleEvent(context, event2));
+         THEN("start again, should return FATAL_BUG") {
+            REQUIRE(Result::FATAL_BUG == action.exec(context));
+         }
+         THEN("event 1 received, should return FATAL_BUG") {
+            REQUIRE(Result::FATAL_BUG == action.handleEvent(context, event1));
+         }
+         THEN("event 2 received, should return FATAL_BUG") {
+            REQUIRE(Result::FATAL_BUG == action.handleEvent(context, event2));
+         }
       }
    };
 
-   FIXTURE(TestConcurrent1) {
+   SCENARIO("__concurrent with 2 sync actions") {
       __concurrent(__sync(SyncAction1), __sync(SyncAction1)) action;
 
       StupidTransactionContext context{};
 
-      TEST("exec should return SUCCESS") {
-         std::cout << sizeof(action) << std::endl;
-         ASSERT_EQ(Result::SUCCESS, action.exec(context));
+      WHEN("exec, should return SUCCESS") {
+         REQUIRE(Result::SUCCESS == action.exec(context));
       }
    };
 
-   FIXTURE(TestConcurrent2) {
+   SCENARIO("__concurrent with a failed action") {
       __concurrent(__asyn(AsyncAction1), __sync(FailedSyncAction4)) action;
 
       StupidTransactionContext context{};
 
-      TEST("exec should return FAILED") {
-         ASSERT_EQ(Result::FAILED, action.exec(context));
+      WHEN("exec, should return FAILED") {
+         REQUIRE(Result::FAILED == action.exec(context));
       }
    };
-
-   using ProcedureAction =
-   __procedure(__asyn(AsyncAction4), __finally(__on_status(Result::FORCE_STOPPED, __asyn(AsyncAction2))));
 
    using ProcedureAction1 =
    __procedure(__asyn(AsyncAction4), __finally(__asyn(AsyncAction2)));
 
-   FIXTURE(TestConcurrent3) {
+   SCENARIO("__concurrent with a failed action & a procedure") {
       __concurrent(ProcedureAction1, __sync(FailedSyncAction4)) action;
 
       StupidTransactionContext context{};
@@ -115,34 +100,38 @@ namespace {
       const EV_NS::ConsecutiveEventInfo eventInfo2{EV_MSG_2, msg2};
       TSL_NS::Event event2{eventInfo2};
 
-      TEST("exec should return FAILED") {
-         ASSERT_EQ(Result::CONTINUE, action.exec(context));
-         ASSERT_EQ(Result::FAILED, action.handleEvent(context, event2));
-      }
+      GIVEN("a clean runtime environment") {
+         REQUIRE(Result::SUCCESS == context.getRuntimeEnvStatus());
+         WHEN("exec, should return CONTINUE, because precedure is stopping") {
+            REQUIRE(Result::CONTINUE == action.exec(context));
+            THEN("should report occured error to environment") {
+               REQUIRE(Result::FAILED == context.getRuntimeEnvStatus());
+            }
+            THEN("if event 2 received, should return FAILED") {
+               REQUIRE(Result::FAILED == action.handleEvent(context, event2));
+            }
 
-      TEST("exec -> stop should return CONTINUE") {
-         ASSERT_EQ(Result::CONTINUE, action.exec(context));
-         ASSERT_EQ(Result::CONTINUE, action.stop(context, Result::OUT_OF_SCOPE));
-      }
-
-      TEST("exec should report Failure") {
-         ASSERT_EQ(Result::SUCCESS, context.getRuntimeEnvStatus());
-         ASSERT_EQ(Result::CONTINUE, action.exec(context));
-         ASSERT_EQ(Result::FAILED, context.getRuntimeEnvStatus());
+            THEN("if stop, should return CONTINUE") {
+               REQUIRE(Result::CONTINUE == action.stop(context, Result::OUT_OF_SCOPE));
+            }
+         }
       }
    };
 
-   FIXTURE(TestConcurrent4) {
+   using ProcedureAction =
+   __procedure(__asyn(AsyncAction4), __finally(__on_status(Result::FORCE_STOPPED, __asyn(AsyncAction2))));
+
+   SCENARIO("__concurrent with a immediate-stopped procedure ") {
       __concurrent(__sync(FailedSyncAction4), ProcedureAction) action;
 
       StupidTransactionContext context{};
 
-      TEST("exec should return FAILED") {
-         ASSERT_EQ(Result::FAILED, action.exec(context));
+      WHEN("exec, should return FAILED") {
+         REQUIRE(Result::FAILED == action.exec(context));
       }
    };
 
-   FIXTURE(TestConcurrent5) {
+   SCENARIO("__concurrent with a failed async action") {
       __concurrent(__asyn(FailedAsyncAction3), ProcedureAction) action;
 
       StupidTransactionContext context{};
@@ -155,59 +144,40 @@ namespace {
       const EV_NS::ConsecutiveEventInfo eventInfo3{EV_MSG_3, msg3};
       TSL_NS::Event event3{eventInfo3};
 
-      TEST("exec should return CONTINUE") {
-         ASSERT_EQ(Result::CONTINUE, action.exec(context));
+      GIVEN("a clean runtime env") {
+         REQUIRE(Result::SUCCESS == context.getRuntimeEnvStatus());
+
+         WHEN("exec, should return CONTINUE") {
+            REQUIRE(Result::CONTINUE == action.exec(context));
+            THEN("the environment keep clean") {
+               REQUIRE(Result::SUCCESS == context.getRuntimeEnvStatus());
+            }
+
+            THEN("if event 3 received, should return CONTINUE") {
+               REQUIRE(Result::CONTINUE == action.handleEvent(context, event3));
+               AND_THEN("should report FAILED to env") {
+                  REQUIRE(Result::FAILED == context.getRuntimeEnvStatus());
+               }
+               AND_THEN("if event 2 received, should return FAILED") {
+                  REQUIRE(Result::FAILED == action.handleEvent(context, event2));
+               }
+            }
+            THEN("if stop, should return CONTINUE") {
+               REQUIRE(Result::CONTINUE == action.stop(context, Result::OUT_OF_SCOPE));
+               AND_WHEN("if event 2 received, should return FORCE_STOPPED") {
+                  REQUIRE(Result::FORCE_STOPPED == action.handleEvent(context, event2));
+               }
+               AND_WHEN("if event 3 received, should return UNKNOWN_EVENT") {
+                  REQUIRE(Result::UNKNOWN_EVENT == action.handleEvent(context, event3));
+               }
+               AND_WHEN("stop again, should return CONTINUE") {
+                  REQUIRE(Result::CONTINUE == action.stop(context, Result::OUT_OF_SCOPE));
+               }
+            }
+
+         }
       }
 
-      TEST("event3 should return CONTINUE") {
-         ASSERT_EQ(Result::CONTINUE, action.exec(context));
-         ASSERT_EQ(Result::CONTINUE, action.handleEvent(context, event3));
-      }
-
-      TEST("event3 should report failture to runtime context") {
-         ASSERT_EQ(Result::SUCCESS, context.getRuntimeEnvStatus());
-         ASSERT_EQ(Result::CONTINUE, action.exec(context));
-         ASSERT_EQ(Result::SUCCESS, context.getRuntimeEnvStatus());
-         ASSERT_EQ(Result::CONTINUE, action.handleEvent(context, event3));
-         ASSERT_EQ(Result::FAILED, context.getRuntimeEnvStatus());
-      }
-
-      TEST("event3 -> event2 should return SUCCESS") {
-         ASSERT_EQ(Result::CONTINUE, action.exec(context));
-         ASSERT_EQ(Result::CONTINUE, action.handleEvent(context, event3));
-         ASSERT_EQ(Result::FAILED, action.handleEvent(context, event2));
-      }
-
-      TEST("stop should return CONTINUE") {
-         ASSERT_EQ(Result::CONTINUE, action.exec(context));
-         context.reportFailure(Result::FAILED);
-         ASSERT_EQ(Result::CONTINUE, action.stop(context, Result::OUT_OF_SCOPE));
-      }
-
-      TEST("if not error on runtime-context, stop should return FORCE_STOPPED") {
-         ASSERT_EQ(Result::CONTINUE, action.exec(context));
-         ASSERT_EQ(Result::CONTINUE, action.stop(context, Result::OUT_OF_SCOPE));
-         ASSERT_EQ(Result::FORCE_STOPPED, action.handleEvent(context, event2));
-      }
-
-      TEST("after stop, event2 should return FORCE_STOPPED") {
-         ASSERT_EQ(Result::CONTINUE, action.exec(context));
-         ASSERT_EQ(Result::CONTINUE, action.stop(context, Result::OUT_OF_SCOPE));
-         ASSERT_EQ(Result::FORCE_STOPPED, action.handleEvent(context, event2));
-      }
-
-      TEST("after stop, event3 should return UNKNOWN_EVENT") {
-         ASSERT_EQ(Result::CONTINUE, action.exec(context));
-         context.reportFailure(Result::FAILED);
-         ASSERT_EQ(Result::CONTINUE, action.stop(context, Result::OUT_OF_SCOPE));
-         ASSERT_EQ(Result::UNKNOWN_EVENT, action.handleEvent(context, event3));
-      }
-
-      TEST("after stop, stop again should return CONTINUE") {
-         ASSERT_EQ(Result::CONTINUE, action.exec(context));
-         ASSERT_EQ(Result::CONTINUE, action.stop(context, Result::TIMEDOUT));
-         ASSERT_EQ(Result::CONTINUE, action.stop(context, Result::TIMEDOUT));
-      }
    };
 
    using ProcedureAction2 =
@@ -215,7 +185,7 @@ namespace {
       __asyn(AsyncAction4),
       __finally(__on_status(Result::FORCE_STOPPED, __asyn(AsyncAction2))));
 
-   FIXTURE(TestConcurrent6) {
+   SCENARIO("__concurrent without internal error") {
       __concurrent(__asyn(FailedAsyncAction3), ProcedureAction2) action;
 
       StupidTransactionContext context{};
@@ -224,10 +194,12 @@ namespace {
       const EV_NS::ConsecutiveEventInfo eventInfo2{EV_MSG_2, msg2};
       TSL_NS::Event event2{eventInfo2};
 
-      TEST("after stop, event3 should return UNKNOWN_EVENT") {
-         ASSERT_EQ(Result::CONTINUE, action.exec(context));
-         ASSERT_EQ(Result::CONTINUE, action.stop(context, Result::OUT_OF_SCOPE));
-         ASSERT_EQ(Result::FORCE_STOPPED, action.handleEvent(context, event2));
+      REQUIRE(Result::CONTINUE == action.exec(context));
+      WHEN("after stop, event3 should return CONTINUE") {
+         REQUIRE(Result::CONTINUE == action.stop(context, Result::OUT_OF_SCOPE));
+         AND_WHEN("event 2 received, should return FORCE_STOPPED") {
+            REQUIRE(Result::FORCE_STOPPED == action.handleEvent(context, event2));
+         }
       }
    };
 
@@ -236,7 +208,7 @@ namespace {
       __asyn(FailedAsyncAction3),
       __finally(__asyn(AsyncAction2)));
 
-   FIXTURE(TestConcurrent7) {
+   SCENARIO("__concurrent ") {
       __concurrent(__asyn(AsyncAction1), ProcedureAction3) action;
 
       const Msg1 msg1{ 10, 20 };
@@ -252,12 +224,16 @@ namespace {
       TSL_NS::Event event3{eventInfo3};
 
       StupidTransactionContext context{};
+      REQUIRE(Result::CONTINUE == action.exec(context));
 
-      TEST("should not process AsyncAction1 if event3 arrives first") {
-         ASSERT_EQ(Result::CONTINUE, action.exec(context));
-         ASSERT_EQ(Result::CONTINUE, action.handleEvent(context, event3));
-         ASSERT_EQ(Result::UNKNOWN_EVENT, action.handleEvent(context, event1));
-         ASSERT_EQ(Result::FAILED, action.handleEvent(context, event2));
+      WHEN("event 3 received, should return CONTINUE") {
+         REQUIRE(Result::CONTINUE == action.handleEvent(context, event3));
+         AND_WHEN("event 1 received, should return UNKNOWN_EVENT") {
+            REQUIRE(Result::UNKNOWN_EVENT == action.handleEvent(context, event1));
+            AND_THEN("if event2 received, should return FAILED") {
+               REQUIRE(Result::FAILED == action.handleEvent(context, event2));
+            }
+         }
       }
    };
 
@@ -271,7 +247,7 @@ namespace {
       __sync(SyncAction2),
       __finally(__asyn(AsyncAction1)));
 
-   FIXTURE(TestConcurrent8) {
+   SCENARIO("__concurrent with") {
       __concurrent(ProcedureAction4, ProcedureAction5) action;
 
       const Msg1 msg1{ 10, 20 };
@@ -284,38 +260,48 @@ namespace {
 
       StupidTransactionContext context{};
 
-      TEST("stop doesn't work") {
-         ASSERT_EQ(Result::CONTINUE, action.exec(context));
-         ASSERT_EQ(Result::CONTINUE, action.stop(context, Result::OUT_OF_SCOPE));
-         ASSERT_EQ(Result::CONTINUE, action.handleEvent(context, event1));
-         ASSERT_EQ(Result::SUCCESS, action.handleEvent(context, event2));
+      REQUIRE(Result::CONTINUE == action.exec(context));
+
+      WHEN("stop, should return CONTINUE") {
+         REQUIRE(Result::CONTINUE == action.stop(context, Result::OUT_OF_SCOPE));
+         AND_WHEN("event 1 received, should return CONTINUE") {
+            REQUIRE(Result::CONTINUE == action.handleEvent(context, event1));
+            AND_THEN("if event 2 received, should return SUCCESS") {
+               REQUIRE(Result::SUCCESS == action.handleEvent(context, event2));
+            }
+         }
+         AND_WHEN("event 2 received, should return CONTINUE") {
+            REQUIRE(Result::CONTINUE == action.handleEvent(context, event2));
+            AND_THEN("if event 1 received, should return SUCCESS") {
+               REQUIRE(Result::SUCCESS == action.handleEvent(context, event1));
+            }
+         }
       }
 
-      TEST("stop doesn't work 2") {
-         ASSERT_EQ(Result::CONTINUE, action.exec(context));
-         ASSERT_EQ(Result::CONTINUE, action.handleEvent(context, event1));
-         ASSERT_EQ(Result::CONTINUE, action.stop(context, Result::OUT_OF_SCOPE));
-         ASSERT_EQ(Result::SUCCESS, action.handleEvent(context, event2));
+      WHEN("event 1 received, should return CONTINUE") {
+         REQUIRE(Result::CONTINUE == action.handleEvent(context, event1));
+         WHEN("stop, should return CONTINUE") {
+            REQUIRE(Result::CONTINUE == action.stop(context, Result::OUT_OF_SCOPE));
+            AND_THEN("if event 2 received, should return SUCCESS") {
+               REQUIRE(Result::SUCCESS == action.handleEvent(context, event2));
+            }
+         }
+         WHEN("event 2 received, should return SUCCESS") {
+            REQUIRE(Result::SUCCESS == action.handleEvent(context, event2));
+         }
       }
 
-      TEST("stop doesn't work in any order") {
-         ASSERT_EQ(Result::CONTINUE, action.exec(context));
-         ASSERT_EQ(Result::CONTINUE, action.stop(context, Result::OUT_OF_SCOPE));
-         ASSERT_EQ(Result::CONTINUE, action.handleEvent(context, event2));
-         ASSERT_EQ(Result::SUCCESS, action.handleEvent(context, event1));
-      }
-
-      TEST("stop doesn't work in any order 2") {
-         ASSERT_EQ(Result::CONTINUE, action.exec(context));
-         ASSERT_EQ(Result::CONTINUE, action.handleEvent(context, event2));
-         ASSERT_EQ(Result::CONTINUE, action.stop(context, Result::OUT_OF_SCOPE));
-         ASSERT_EQ(Result::SUCCESS, action.handleEvent(context, event1));
-      }
-
-      TEST("normal-case") {
-         ASSERT_EQ(Result::CONTINUE, action.exec(context));
-         ASSERT_EQ(Result::CONTINUE, action.handleEvent(context, event1));
-         ASSERT_EQ(Result::SUCCESS, action.handleEvent(context, event2));
+      WHEN("event 2 received, should return CONTINUE") {
+         REQUIRE(Result::CONTINUE == action.handleEvent(context, event2));
+         WHEN("stop, should return CONTINUE") {
+            REQUIRE(Result::CONTINUE == action.stop(context, Result::OUT_OF_SCOPE));
+            AND_THEN("if event 1 received, should return SUCCESS") {
+               REQUIRE(Result::SUCCESS == action.handleEvent(context, event1));
+            }
+         }
+         WHEN("event 1 received, should return SUCCESS") {
+            REQUIRE(Result::SUCCESS == action.handleEvent(context, event1));
+         }
       }
    };
 }

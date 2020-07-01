100e9db9f8a05c6002774ef6691646c9f650220d
Darwin Yuan <darwin_yuan@hotmail.com>
2020-07-01 13:33:10 +0800

    added SchedFork

--- include/trans-dsl/sched/domain/MultiThreadContext.h
+++ include/trans-dsl/sched/domain/MultiThreadContext.h
@@ -8,11 +8,15 @@
 #include <cub/dci/Role.h>
 #include <trans-dsl/tsl_status.h>
 #include <trans-dsl/sched/domain/ThreadBitMap.h>
+#include <trans-dsl/sched/domain/ThreadId.h>
 
 TSL_NS_BEGIN
 
+struct SchedAction;
+
 DEFINE_ROLE(MultiThreadContext) {
    ABSTRACT(join(ThreadBitMap) -> ThreadBitMap);
+   ABSTRACT(startThread(ThreadId, SchedAction&) -> Status);
 };
 
 TSL_NS_END
---
+++ src/sched/action/SchedFork.cpp
@@ -0,0 +1,23 @@
+//
+// Created by Darwin Yuan on 2020/7/1.
+//
+
+#include <trans-dsl/sched/action/SchedFork.h>
+#include <trans-dsl/utils/ActionStatus.h>
+#include <trans-dsl/sched/domain/TransactionContext.h>
+#include <trans-dsl/tsl_config.h>
+#include <trans-dsl/sched/domain/MultiThreadContext.h>
+
+TSL_NS_BEGIN
+
+auto SchedFork::exec(TransactionContext& context) -> Status {
+   auto mt = context.getMultiThreadContext();
+   unlikely_branch
+   if(unlikely(mt == nullptr)) {
+      return Result::FATAL_BUG;
+   }
+
+   return mt->startThread(getThreadId(), getThreadAction());
+}
+
+TSL_NS_END

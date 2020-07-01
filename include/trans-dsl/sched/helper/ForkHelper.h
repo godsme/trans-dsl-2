defbb21218e85e5d249745d1784f3f3e96cfcb58
Darwin Yuan <darwin_yuan@hotmail.com>
2020-07-01 13:42:27 +0800

    added ForkHelper

---
+++ include/trans-dsl/sched/helper/ForkHelper.h
@@ -0,0 +1,42 @@
+//
+// Created by Darwin Yuan on 2020/7/1.
+//
+
+#ifndef TRANS_DSL_2_FORKHELPER_H
+#define TRANS_DSL_2_FORKHELPER_H
+
+#include <trans-dsl/tsl_config.h>
+#include <trans-dsl/sched/domain/ThreadId.h>
+#include <trans-dsl/sched/action/SchedFork.h>
+#include <trans-dsl/sched/concepts/ConceptHelper.h>
+#include <trans-dsl/sched/domain/ThreadBitMap.h>
+#include <trans-dsl/sched/concepts/SchedActionConcept.h>
+
+TSL_NS_BEGIN
+
+   namespace details {
+      template <ThreadId TID, CONCEPT(SchedActionConcept) T_ACTION>
+      class Fork final : public SchedFork {
+         static_assert(TID < sizeof(ThreadBitMap) * 8, "specified Thread ID is out of scope");
+         static_assert(TID != 0, "0 is reserved for main thread");
+         #if !__CONCEPT_ENABLED
+         static_assert(SchedActionConcept<T_ACTION>, "the forked thread must be an Action");
+         #endif
+
+      private:
+         OVERRIDE(getThreadId() const -> ThreadId) {
+            return TID;
+         }
+         OVERRIDE(getThreadAction() -> SchedAction&) {
+            return action;
+         }
+
+         T_ACTION action;
+      };
+   }
+
+#define __fork(tid, ...) TLS_NS::details::Fork<tid, __VA_ARGS__>
+
+TSL_NS_END
+
+#endif //TRANS_DSL_2_FORKHELPER_H
--- include/trans-dsl/trans-dsl.h
+++ include/trans-dsl/trans-dsl.h
@@ -18,6 +18,7 @@
 #include <trans-dsl/sched/helper/WaitHelper.h>
 #include <trans-dsl/sched/helper/ThrowHelper.h>
 #include <trans-dsl/sched/helper/FragmentHelper.h>
+#include <trans-dsl/sched/helper/ForkHelper.h>
 #include <trans-dsl/sched/helper/JoinHelper.h>
 
 #endif //TRANS_DSL_2_TRANS_DSL_H
